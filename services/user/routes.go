8a6e82bc7e94a030b1b91cc73579a274134eb603
Xenos Warlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-12-07 20:18:06 +0530

    Add files via upload

--- services/user/routes.go
+++ services/user/routes.go
@@ -1,8 +1,12 @@
 package user
 
 import (
+	"fmt"
 	"net/http"
 
+	customtypes "github.com/XenosWarlocks/NestCart/types"
+	"github.com/XenosWarlocks/NestCart/utils"
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 )
 
@@ -15,7 +19,7 @@ func NewHandler() *Handler {
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/login", h.handleLogin).Methods("POST")
-	router.HandleFunc("/register", h.handleLogin).Methods("POST")
+	router.HandleFunc("/register", h.handleRegister).Methods("POST")
 }
 
 func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
@@ -23,5 +27,15 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
-	// Handle registration logic here
+	var user customtypes.RegisterUserPayload
+	if err := utils.ParseJSON(r, &user); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := utils.Validate.Struct(user); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
+		return
+	}
 }

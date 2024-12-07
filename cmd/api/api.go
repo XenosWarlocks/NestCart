b01483211b59fa0b99efd9f27008d41da21211b0
Xenos Warlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-12-07 16:56:43 +0530

    Add files via upload

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/XenosWarlocks/NestCart/services/user"
 	"github.com/gorilla/mux"
 )
 
@@ -20,9 +21,12 @@ func NewAPIServer(addr string, db *sql.DB) *APIServer {
 	}
 }
 
-func (s, *APIServer) Run() error {
+func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subrouter)
 
 	log.Println("Listening on", s.addr)
 
--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,25 @@ import (
 	"log"
 
 	"github.com/XenosWarlocks/NestCart/cmd/api"
+	"github.com/XenosWarlocks/NestCart/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIServer("8080", nil)
+	cfg := mysql.Config{
+		User:                 "roots",
+		Passwd:               "password",
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               "XXXXXXXX",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	}
+	db, err := db.NewMySQLStorage(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	server := api.NewAPIServer(":8080", nil)
 	if err := server.Run(); err != nil {
 		log.Fatal()
 	}

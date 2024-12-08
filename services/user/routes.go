287b2041ed8311207481876cf318cc0a1b20d83d
Xenos Warlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-12-08 18:40:46 +0530

    Add files via upload

--- services/user/routes.go
+++ services/user/routes.go
@@ -4,16 +4,17 @@ import (
 	"fmt"
 	"net/http"
 
-	customtypes "github.com/XenosWarlocks/NestCart/types"
+	"github.com/XenosWarlocks/NestCart/types"
 	"github.com/XenosWarlocks/NestCart/utils"
 	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 )
 
 type Handler struct {
+	store types.UserStore
 }
 
-func NewHandler() *Handler {
+func NewHandler(Store types.UserStore) *Handler {
 	return &Handler{}
 }
 
@@ -27,7 +28,7 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
-	var user customtypes.RegisterUserPayload
+	var user types.RegisterUserPayload
 	if err := utils.ParseJSON(r, &user); err != nil {
 		utils.WriteError(w, http.StatusBadRequest, err)
 		return
@@ -36,6 +37,13 @@ func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 	if err := utils.Validate.Struct(user); err != nil {
 		errors := err.(validator.ValidationErrors)
 		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
+		return
+	}
+
+	// check if user exists
+	_, err := h.store.GetUserByEmail(user.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", user.Email))
 		return
 	}
 }
